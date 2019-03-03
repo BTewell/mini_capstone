75667422dd077743aae3323701e6cb7c737187d9
“Brett <bretttewell@gmail.com>
2019-03-02 21:26:40 -0600

    add frontend file for user input (incomplete)

---
+++ frontend.rb
@@ -0,0 +1,4 @@
+require http
+
+system "clear"
+p "Welcome to The Emerald Talon! Your one-stop-shop for all things gaming (so long as your gaming is containted to 2nd Edition Advanced Dungeons & Dragons)! 
