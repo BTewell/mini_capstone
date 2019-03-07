19505cff0d023e5b9c6f55fa101a0b4169b1dba2
“Brett <bretttewell@gmail.com>
2019-03-07 12:12:28 -0600

    add mini capstone app validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates :description, presence: true
+  validates :description, length: {in: 10..500}
+  validates :image_url, presence: true
+
   def bargain_finder?
     price < 10
   end
